92a41f33eecc3591280a3c57bda59881de3ec70b
Mario90 <mgpinan1990@gmail.com>
2020-08-02 13:13:49 +0100

    hello world rout tested

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//Routes
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("hello world");
+});
+
+//Create
+
+//Read
+
+//Update
+
+//Delete
+
+//export app to firebase cloud
+exports.app = functions.https.onRequest(app);
