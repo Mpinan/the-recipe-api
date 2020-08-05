c33319db7d5f1a6323323f48ef5614f0ef6fd9b1
Mario90 <mgpinan1990@gmail.com>
2020-08-05 22:30:56 +0100

    -

--- functions/routes/hello-world.js
+++ functions/routes/hello-world.js
@@ -1,27 +1,27 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+// const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
 
-var serviceAccount = require("../functions/permissions.json");
+// var serviceAccount = require("../functions/permissions.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://the-recipe-api.firebaseio.com",
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://the-recipe-api.firebaseio.com",
+// });
 
-const express = require("express");
-const app = express();
-const db = admin.firestore();
+// const express = require("express");
+// const app = express();
+// const db = admin.firestore();
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: true,
-  })
-);
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: true,
+//   })
+// );
 
-//Routes
-app.get("/hello-world", (req, res) => {
-  return res.status(200).send("hello world");
-});
+// //Routes
+// app.get("/hello-world", (req, res) => {
+//   return res.status(200).send("hello world");
+// });
 
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app);
